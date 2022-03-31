41cac3a5026ede8bda964277d4da1b0128b18f7a
Bhaskar <arbkm22@gmail.com>
2022-03-31 22:44:52 +0530

    added cast and movie_detail data class and modified movie_list

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+from lib2to3.pytree import Base
+from turtle import st
 from typing import List
 from pydantic import BaseModel
+
 
 class Torrents(BaseModel):
     url: str
@@ -9,7 +12,7 @@ class Torrents(BaseModel):
     seeds: int 
     peers: int 
     size: str
-    
+
 
 class MovieList(BaseModel):
     id: int
@@ -25,5 +28,22 @@ class MovieList(BaseModel):
     language: str
     mpa_rating: str
     background_image_original: str
+    medium_cover_image: str
     large_cover_image: str
     torrents: List[Torrents]
+
+
+class Cast(BaseModel):
+    name: str
+    character_name: str
+    url_image: str
+    imdb_code: str
+
+
+class MovieDetail(MovieList):
+    download_count: int
+    like_count: int
+    cast: List[Cast]
+    lsi1: str
+    lsi2: str
+    lsi3: str
