7fc31914cd7ca3fbad0e35a43b46b9da981b0da9
Bhaskar <arbkm22@gmail.com>
2022-04-01 12:49:22 +0530

    added routes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+from helpers import get_movie, get_movie_list
+
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"hello": "world"}
+
+@app.get("/movieList")
+async def movieList(p: int = 1):
+    return get_movie_list(p)
+
+@app.get("/movieDetail")
+async def movieDetail(id: int):
+    return get_movie(id)
