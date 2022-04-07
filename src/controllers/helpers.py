0f8413a2128358a85dcd8b2b6bc50f42ea8157ed
Bhaskar <arbkm22@gmail.com>
2022-04-07 16:52:31 +0530

    null check

--- src/controllers/helpers.py
+++ src/controllers/helpers.py
@@ -8,26 +8,26 @@ def get_movie(id: int) -> MovieDetail:
     if (response.status_code == 200):
         movie = response.json()["data"]["movie"]
         movie_detail = MovieDetail(
-            id = movie["id"],
-            url = movie["url"],
-            imdb_code = movie["imdb_code"],
-            title_eng = movie["title_english"],
-            year = movie["year"],
-            rating = movie["rating"],
-            genres = movie["genres"],
-            large_cover_image = movie["large_cover_image"],
-            download_count = movie["download_count"],
-            like_count = movie["like_count"],
-            cast = movie["cast"],
-            runtime = movie["runtime"],
-            description_full = movie["description_full"],
-            yt_trailer_code = movie["yt_trailer_code"],
-            language = movie["language"],
-            background_image_original = movie["background_image_original"],
-            lsi1 = movie["large_screenshot_image1"],
-            lsi2 = movie["large_screenshot_image2"],
-            lsi3 = movie["large_screenshot_image3"],
-            torrents = movie["torrents"]
+            id = movie.get("id"),
+            url = movie.get("url"),
+            imdb_code = movie.get("imdb_code"),
+            title_eng = movie.get("title_english"),
+            year = movie.get("year"),
+            rating = movie.get("rating"),
+            genres = movie.get("genres"),
+            large_cover_image = movie.get("large_cover_image"),
+            download_count = movie.get("download_count"),
+            like_count = movie.get("like_count"),
+            cast = movie.get("cast"),
+            runtime = movie.get("runtime"),
+            description_full = movie.get("description_full"),
+            yt_trailer_code = movie.get("yt_trailer_code"),
+            language = movie.get("language"),
+            background_image_original = movie.get("background_image_original"),
+            lsi1 = movie.get("large_screenshot_image1"),
+            lsi2 = movie.get("large_screenshot_image2"),
+            lsi3 = movie.get("large_screenshot_image3"),
+            torrents = movie.get("torrents")
         )
         return movie_detail
     else:
@@ -41,14 +41,14 @@ def get_movie_list(page: int = 1):
         movies = []
         for movie in data:
             movie_data = MovieList(
-                id = movie["id"],
-                url = movie["url"],
-                imdb_code = movie["imdb_code"],
-                title_eng = movie["title_english"],
-                year = movie["year"],
-                rating = movie["rating"],
-                genres = movie["genres"],
-                large_cover_image = movie["large_cover_image"]
+                id = movie.get("id"),
+                url = movie.get("url"),
+                imdb_code = movie.get("imdb_code"),
+                title_eng = movie.get("title_english"),
+                year = movie.get("year"),
+                rating = movie.get("rating"),
+                genres = movie.get("genres"),
+                large_cover_image = movie.get("large_cover_image")
             )
             movies.append(movie_data)
         return movies
