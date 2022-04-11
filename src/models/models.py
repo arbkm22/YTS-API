f913690479ebf78160592b4fc78d20ac44f0633c
Bhaskar <arbkm22@gmail.com>
2022-04-11 21:56:52 +0530

    updated the data class and added new routes

--- src/models/models.py
+++ src/models/models.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+import enum
+from turtle import title
 from typing import List, Optional
 from pydantic import BaseModel
 
@@ -11,17 +14,50 @@ class Torrents(BaseModel):
     peers: int 
     size: str
 
-
-class MovieList(BaseModel):
-    id: int
-    url: str
-    imdb_code: Optional[str]
+@dataclass
+class MovieList():
+    #id: int
+    #url: str
+    #imdb_code: Optional[str]
     title_eng: Optional[str]
     year: Optional[int]
-    rating: Optional[float]
-    genres: Optional[List[str]]
-    large_cover_image: Optional[str]
+    #rating: Optional[float]
+    #genres: Optional[List[str]]
+    #large_cover_image: Optional[str]
+    medium_cover_image: Optional[str]
 
+    # def __init__(self, id, url, imdb_code, title_eng, year, 
+    #     rating, genres, large_cover_image):
+    #     self.id = id
+    #     self.url = url
+    #     self.imdb_code = imdb_code
+    #     self.title_eng = title_eng
+    #     self.year = year
+    #     self.rating = rating
+    #     self.genres = genres
+    #     self.large_cover_image = large_cover_image
+    #     self.index = dict({
+    #         "id": self.id,
+    #         "url": self.url,
+    #         "imdb_code": self.imdb_code,
+    #         "title_eng": self.title_eng,
+    #         "year": self.year,
+    #         "rating": self.rating,
+    #         "genres": self.genres,
+    #         "large_cover_image": self.large_cover_image
+    #     })
+    def __init__(self, title_eng, year, medium_cover_image):
+        self.title_eng = title_eng
+        self.year = year
+        self.medium_cover_image = medium_cover_image
+        self.index = dict({
+            "title_eng": self.title_eng,
+            "year": self.year,
+            "medium_cover_image": self.medium_cover_image
+        })
+
+    def __getitem__(self, index):
+        return self.index[index]
 
 class Cast(BaseModel):
     name: str
--- src/routes/yts.py
+++ src/routes/yts.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from src.controllers.helpers import get_movie, get_movie_list
+from src.controllers.helpers_uno import get_latest_movies, get_most_liked_movies, get_popular_movies
 
 router = APIRouter()
 
@@ -18,3 +19,15 @@ async def movies_list(p: int = 1):
 @router.get("/movie_detail")
 async def movie_detail(id: int):
     return get_movie(id)
+
+@router.get("/latest_movies")
+async def latest_movies():
+    return get_latest_movies()
+
+@router.get("/popular_movies")
+async def popular_movies():
+    return get_popular_movies()
+
+@router.get("/most_liked")
+async def most_liked():
+    return get_most_liked_movies()
